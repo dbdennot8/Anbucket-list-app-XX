e08f72048c47434fb75a573d9c192d856b36a340
dbdennot8 <dennisbmithamo@gmail.com>
2017-07-18 14:03:56 +0300

    Include app's functionality and tests

---
+++ functionality-and-tests/test_cases.py
@@ -0,0 +1,8 @@
+'''Unittesting the Bucket List app'''
+
+
+import unittest
+
+class TestBucketListfeatures(unittest.TestCase):
+    '''Defines a class which once initialized allows testing to be done on app's methods'''
+    
